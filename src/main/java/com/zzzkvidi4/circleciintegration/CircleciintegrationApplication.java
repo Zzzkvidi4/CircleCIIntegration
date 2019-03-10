c4267782bf68218b5aa2735fa10c634e96ceed53
Zzzkvidi4 <zzzkvidi4@gmail.com>
2019-03-10 20:36:56 +0300

    Initial commit

---
+++ src/main/java/com/zzzkvidi4/circleciintegration/CircleciintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.zzzkvidi4.circleciintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleciintegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzzkvidi4/circleciintegration/CircleciintegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zzzkvidi4.circleciintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CircleciintegrationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
