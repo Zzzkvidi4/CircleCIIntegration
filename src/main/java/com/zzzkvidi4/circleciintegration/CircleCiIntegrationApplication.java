85246b23edf38f0fd8cd0dd23670cdd5757deca6
Zzzkvidi4 <zzzkvidi4@gmail.com>
2019-03-10 22:45:00 +0300

    Added checkstyle tests

---
+++ src/main/java/com/zzzkvidi4/circleciintegration/CircleCiIntegrationApplication.java
@@ -0,0 +1,20 @@
+package com.zzzkvidi4.circleciintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Стартовый класс для приложения.
+ */
+@SpringBootApplication
+public class CircleCiIntegrationApplication {
+
+    /**
+     * Стартовый метод для запуска приложения.
+     *
+     * @param args параметры запуска.
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(CircleCiIntegrationApplication.class, args);
+    }
+}
---
+++ src/main/java/com/zzzkvidi4/circleciintegration/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Приложение для проверки настройки CI, тестов и автоматического деплоя.
+ */
+package com.zzzkvidi4.circleciintegration;
